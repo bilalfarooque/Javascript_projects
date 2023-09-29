92413028797c33eaa28cc74635c68759d550e12c
Bilal Khatri <engrbilalfarooque@gmail.com>
2023-09-29 10:27:05 +0500

    calender added

--- Digital_clock/app.js
+++ Digital_clock/app.js
@@ -1,15 +1,31 @@
 let hr = document.getElementById("hr");
 let min = document.getElementById("min");
 let sec = document.getElementById("sec");
+let period = document.getElementById("period");
+let day = document.getElementById("day");
+let mon = document.getElementById("mon");
+let date = document.getElementById("date");
+let year = document.getElementById("year");
 
+setInterval(() => {
+  let currentTime = new Date();
+  let hours = currentTime.getHours();
 
-setInterval(()=>{
-    let currentTime = new Date();
-    
-    hr.innerHTML = (currentTime.getHours()<10? "0":"" )+ currentTime.getHours();
-    min.innerHTML = (currentTime.getMinutes()<10? "0":"" )+ currentTime.getMinutes();
-    sec.innerHTML = (currentTime.getSeconds()<10? "0":"" )+ currentTime.getSeconds();
+  //set time period AM/PM
+  period.innerHTML = hours >= 12 ? (period = "PM") : (period = "AM");
 
-},100)
+  //set 12hour format
+  hours = hours > 12 ? hours - 12 : hours;
 
+  // zero before padding
+  hr.innerHTML = (hours < 10 ? "0" : "") + hours;
+  min.innerHTML =
+    (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
+  sec.innerHTML =
+    (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
 
+  day.innerHTML = currentTime.toLocaleString("default", { weekday: "long" });
+  mon.innerHTML = currentTime.toLocaleString("default", { month: "short" });
+  year.innerHTML = currentTime.getFullYear();
+  date.innerHTML = currentTime.getDate();
+}, 100);
