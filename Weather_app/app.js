23036d3907e38af3e9074998384369e42334805b
Bilal Khatri <engrbilalfarooque@gmail.com>
2023-09-30 23:52:59 +0500

    weather video added

--- Weather_app/app.js
+++ Weather_app/app.js
@@ -5,6 +5,9 @@ const apiUrl =
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search img");
 const weatherIcon = document.querySelector(".weather-icon");
+const backVideo = document.querySelector(".back-video");
+
+const weather = document.querySelector(".weather");
 
 async function checkWeather(cityName) {
   const response = await fetch(apiUrl + cityName + `&appid=${apiKey}`);
@@ -30,30 +33,78 @@ async function checkWeather(cityName) {
     //set image according to the weather from api
     if (data.weather[0].main == "Clouds") {
       weatherIcon.src = "./assets/clouds.png";
+      backVideo.src = "./assets/videos/cloudy.mp4";
     } else if (data.weather[0].main == "Haze") {
       weatherIcon.src = "./assets/haze.png";
+      backVideo.src = "./assets/videos/haze.mp4";
     } else if (data.weather[0].main == "Clear") {
       weatherIcon.src = "./assets/clear.png";
+      backVideo.src = "./assets/videos/clear.mp4";
     } else if (data.weather[0].main == "Rain") {
       weatherIcon.src = "./assets/rain.png";
+      backVideo.src = "./assets/videos/rain.mp4";
     } else if (data.weather[0].main == "Drizzle") {
       weatherIcon.src = "./assets/drizzle.png";
+      backVideo.src = "./assets/videos/drizzle.mp4";
     } else if (data.weather[0].main == "Mist") {
       weatherIcon.src = "./assets/mist.png";
+      backVideo.src = "./assets/videos/mist.mp4";
     } else if (data.weather[0].main == "Snow") {
       weatherIcon.src = "./assets/snowflake.png";
+      backVideo.src = "./assets/videos/snow.mp4";
     } else if (data.weather[0].main == "Sunny") {
       weatherIcon.src = "./assets/clear.png";
+      backVideo.src = "./assets/videos/clear.mp4";
+    } else if (data.weather[0].main == "Storm") {
+      weatherIcon.src = "./assets/storm.png";
+      backVideo.src = "./assets/videos/storm.mp4";
     }
+
+    
     document.querySelector(".error").style.visibility = "hidden";
     document.querySelector(".weather").style.visibility = "visible";
     weatherIcon.style.display = "block";
+
+   
   }
 }
 
 searchBtn.addEventListener("click", () => {
   checkWeather(searchBox.value);
 });
+
+
+
+
+// //checking condition for save
+// listBox.addEventListener("click", function (e) {
+//     if (e.target.tagName === "LI") {
+//       e.target.classList.toggle("checked");
+//       saveData();
+//     }
+//     else if(e.target.tagName === "SPAN"){
+//       e.target.parentElement.remove();
+//       saveData();
+//     }
+//   },false);
+  
+//   //saving data to avoid lost on reload
+//   function saveData(){
+//       localStorage.setItem('data',listBox.innerHTML);
+//   }
+  
+//   //show data saved
+  
+//   function showData(){
+//       listBox.innerHTML = localStorage.getItem('data')
+//   }
+  
+//   showData();
+
+
+
+
+
 
 // fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/New%20York%20City%2CNY?unitGroup=us&key=19f1c7646f1ca562a5b55173eb3a3e4e&contentType=json", {
 //   method: 'GET',
