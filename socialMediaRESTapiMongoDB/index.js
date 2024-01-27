875fc2f2bcbb2a575c477939f09893aea04b36fc
Bilal Khatri <engrbilalfarooque@gmail.com>
2024-01-27 12:52:45 +0500

    auth done

--- socialMediaRESTapiMongoDB/index.js
+++ socialMediaRESTapiMongoDB/index.js
@@ -1,7 +1,27 @@
 import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+import cors from "cors"
+import helmet from "helmet"
+import morgan from "morgan"
+import authRouter from "./routes/authRoute.js"
+import { dbConnection } from "./utils/config.js"
+
+dotenv.config()
+
+dbConnection()
 
 const app = express()
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`)
+//middleware
+app.use(express.json())
+// app.use(helmet())
+app.use(morgan('dev'))
+
+app.use("/auth",authRouter)
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is running on port ${process.env.PORT}`)
 })
+
